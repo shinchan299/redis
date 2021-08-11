cc840b89428e63b5cb1c34c116a5e5b5b881cf37
hujiajing <hujiajing.xdu@outlook.com>
2021-08-11 11:30:46 +0800

    sds.h

--- src/sds.h
+++ src/sds.h
@@ -30,10 +30,20 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+// 此文件中主要为 redis 底层 string 的具体实现方式
+// redis 底层的 string 与 golang 中 slice 较为类似。
+// 本文件中存在一些 C 语言知识点，此处将会一起总结
+//  1. extern 关键字
+//  2. __attribute__((__packed__))
+//  3. ##, 用于字符串的连接
+
 #ifndef __SDS_H
 #define __SDS_H
 
-#define SDS_MAX_PREALLOC (1024*1024)
+// SDS 最大长度为 1M
+#define SDS_MAX_PREALLOC (1024 * 1024)
+
+// extern可以置于变量或者函数前，以标示变量或者函数的定义在别的文件中，提示编译器遇到此变量和函数时在其他模块中寻找其定义。此外extern也可用来进行链接指定。
 extern const char *SDS_NOINIT;
 
 #include <sys/types.h>
@@ -44,6 +54,33 @@ typedef char *sds;
 
 /* Note: sdshdr5 is never used, we just access the flags byte directly.
  * However is here to document the layout of type 5 SDS strings. */
+// __attribute__ ((__packed__)) 这种方式可以取消字节对齐
+// eg: 
+// struct __attribute__((__packed__)) sds {
+//     char a; 
+//     int b;
+// };
+
+// struct sds1 {
+//     char a;
+//     int b;
+// };
+
+// int main(int argc, char *argv[]) {
+//     printf("the size of sds is %lu\n", sizeof(struct sds)); // print 5
+//     printf("the size of sds1 is %lu\n", sizeof(struct sds1)); // print 8
+//     return 0;
+// }
+
+
+// sdshdr 数据结构为 sds 头部
+// 其中主要分为几个部分：
+// 1. flag 长度一共八位，其中低三位用于确定 header 的类型（type）
+// 2. len, 已经使用的长度
+// 3. alloc 分配的长度，其中不包含 header 的长度
+// 4. char buff， 实际的数据
+
+// 如果有阅读 golang slice 的经历的话，会发现这段其实很像 golang 的 slice/string
 struct __attribute__ ((__packed__)) sdshdr5 {
     unsigned char flags; /* 3 lsb of type, and 5 msb of string length */
     char buf[];
@@ -73,19 +110,65 @@ struct __attribute__ ((__packed__)) sdshdr64 {
     char buf[];
 };
 
+// 一些标志符，用于标识具体类型，和 MASK 搭配使用
 #define SDS_TYPE_5  0
 #define SDS_TYPE_8  1
 #define SDS_TYPE_16 2
 #define SDS_TYPE_32 3
 #define SDS_TYPE_64 4
+
+
 #define SDS_TYPE_MASK 7
 #define SDS_TYPE_BITS 3
+
+// 以下两个宏主要用于获取对应的 HEADER
+// 实际的数据指针减去对饮的 header 长度最后，将会指向 header 的首地址
+// ## 用于在宏中的字符串连接
+//
+// #include <stdio.h>
+// 
+// #define CON(s, N) s##N
+
+// int main(int argc, char *argv[]) {
+//     char *s1 = "abcd";
+//     char *s2 = "efg";
+
+//     // printf abcd
+//     printf("%s\n", CON(s, 1));
+//     // print efg
+//     printf("%s\n", CON(s, 2));
+//     return 0;
+// }
+
 #define SDS_HDR_VAR(T,s) struct sdshdr##T *sh = (void*)((s)-(sizeof(struct sdshdr##T)));
-#define SDS_HDR(T,s) ((struct sdshdr##T *)((s)-(sizeof(struct sdshdr##T))))
+#define SDS_HDR(T, s) ((struct sdshdr##T *)((s) - (sizeof(struct sdshdr##T))))
+
+// 对于 TYPE_5 来说的话，直接向右移动3位就可以获取长度了。
 #define SDS_TYPE_5_LEN(f) ((f)>>SDS_TYPE_BITS)
 
+// 注意到这是一个静态函数：
+// static functions are functions that are only visible to other functions in
+// the same file
+// 另，这是一个内联(inline)函数,
+// 主要用于将函数做原地展开从而实现减少函数栈的开销的作用，
+// 一般情况下主要用于一些较为短小的函数。
+// 
+// inline: The intent of the inline specifier is to serve as a hint for the
+// compiler to perform optimizations, such as function inlining, which usually
+// require the definition of a function to be visible at the call site. The
+// compilers can (and usually do) ignore presence or absence of the inline
+// specifier for the purpose of optimization.
+// inline关键字仅仅是建议编译器做内联展开处理，而不是强制
 static inline size_t sdslen(const sds s) {
+    // 在 sds 内存分布如下所示：
+    // [<--len-->][<--alloc-->][<--flag-->][<------buff------>]
+    // 所以通过 s[-1] 可以获取flag
     unsigned char flags = s[-1];
+
+    // 使用 FLAG 和 MASK 进行与运算，获取对应的类型
+    // 这里：
+    // 1. sds5 直接使用 msb 获取长度
+    // 2. sds8以及以上的，通过header中len获取对应长度
     switch(flags&SDS_TYPE_MASK) {
         case SDS_TYPE_5:
             return SDS_TYPE_5_LEN(flags);
@@ -101,6 +184,7 @@ static inline size_t sdslen(const sds s) {
     return 0;
 }
 
+// 此处逻辑和sdslen基本保持一致，不再赘述
 static inline size_t sdsavail(const sds s) {
     unsigned char flags = s[-1];
     switch(flags&SDS_TYPE_MASK) {
@@ -127,6 +211,8 @@ static inline size_t sdsavail(const sds s) {
     return 0;
 }
 
+// sdssetlen主要用于设置sds长度
+// 还是与上述较为类似
 static inline void sdssetlen(sds s, size_t newlen) {
     unsigned char flags = s[-1];
     switch(flags&SDS_TYPE_MASK) {
@@ -151,6 +237,7 @@ static inline void sdssetlen(sds s, size_t newlen) {
     }
 }
 
+// 长度增长函数
 static inline void sdsinclen(sds s, size_t inc) {
     unsigned char flags = s[-1];
     switch(flags&SDS_TYPE_MASK) {
@@ -176,6 +263,7 @@ static inline void sdsinclen(sds s, size_t inc) {
     }
 }
 
+// 已经分配的长度
 /* sdsalloc() = sdsavail() + sdslen() */
 static inline size_t sdsalloc(const sds s) {
     unsigned char flags = s[-1];
@@ -194,6 +282,7 @@ static inline size_t sdsalloc(const sds s) {
     return 0;
 }
 
+// 设置header中已分配的长度
 static inline void sdssetalloc(sds s, size_t newlen) {
     unsigned char flags = s[-1];
     switch(flags&SDS_TYPE_MASK) {
@@ -215,6 +304,7 @@ static inline void sdssetalloc(sds s, size_t newlen) {
     }
 }
 
+// 一些函数的声明，不再赘述了
 sds sdsnewlen(const void *init, size_t initlen);
 sds sdstrynewlen(const void *init, size_t initlen);
 sds sdsnew(const char *init);
